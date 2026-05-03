4a74d73808481c4f66432f68a15c7c2dbc25936e
Fahad Chaudhary <fahadch2200@gmail.com>
2026-05-03 21:39:45 +0500

    Finalize Stack implementation with peek and display functions
    Added peek method to retrieve the top element without removing it. Enhanced display method to show empty stack message.

--- stack .cpp
+++ stack .cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-// Node class
+// Node class 
 class Node {
 public:
     int data;
@@ -13,7 +13,7 @@ public:
     }
 };
 
-// Stack class
+// Stack class - LIFO (Last In First Out) 
 class Stack {
 private:
     Node* top;
@@ -23,12 +23,14 @@ public:
         top = NULL;
     }
 
+    // add new data to the top
     void push(int val) {
         Node* newNode = new Node(val);
         newNode->next = top;
         top = newNode;
     }
 
+    // remove data at the top
     void pop() {
         if(top == NULL) {
             cout << "Stack Underflow\n";
@@ -39,8 +41,22 @@ public:
         delete temp;
     }
 
+    // check Top element without remove
+    int peek() {
+        if(top == NULL) {
+            cout << "Stack is Empty\n";
+            return -1;
+        }
+        return top->data;
+    }
+
+    // print complete stack
     void display() {
         Node* temp = top;
+        if(temp == NULL) {
+            cout << "Stack is empty." << endl;
+            return;
+        }
         while(temp != NULL) {
             cout << temp->data << " ";
             temp = temp->next;
@@ -52,12 +68,23 @@ public:
 int main() {
     Stack s;
 
+    // Data insert 
     s.push(10);
     s.push(20);
     s.push(30);
 
+    cout << "Initial Stack: ";
     s.display();
 
+    // Peek check 
+    cout << "Current Top Element: " << s.peek() << endl;
+
+    // Data remove 
     s.pop();
+    cout << "After Pop, New Top: " << s.peek() << endl;
+    
+    cout << "Final Stack: ";
     s.display();
+
+    return 0;
 }
